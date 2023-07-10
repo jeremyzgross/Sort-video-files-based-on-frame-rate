b968c8c89fd6e94b59d3161d41f60444d86ae01c
jeremyzgross <jeremyzgross@gmail.com>
2023-07-10 10:33:08 -0600

    Main_select base path

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import shutil
 import ffmpeg
+import tkinter as tk
+from tkinter import filedialog
 from constants import BASE_PATH
-
 
 def get_video_frame_rate(video_path):
     probe = ffmpeg.probe(video_path)
@@ -12,7 +13,6 @@ def get_video_frame_rate(video_path):
         return frame_rate
     else:
         return None
-
 
 def create_frame_rate_directories(video_files):
     frame_rates = set()
@@ -27,7 +27,6 @@ def create_frame_rate_directories(video_files):
         os.makedirs(new_directory, exist_ok=True)
 
     return frame_rates
-
 
 def move_files_to_directories(video_files, frame_rates):
     moved_files = []  # List to store the moved file paths
@@ -46,11 +45,22 @@ def move_files_to_directories(video_files, frame_rates):
     for video_file, destination_path in moved_files:
         print(f"Moved file: {video_file} -> {destination_path}")
 
-
 def sort_videos():
     video_files = [filename for filename in os.listdir(BASE_PATH) if filename.endswith((".mp4", ".mov", ".mxf"))]
     frame_rates = create_frame_rate_directories(video_files)
     move_files_to_directories(video_files, frame_rates)
 
+def select_base_path():
+    root = tk.Tk()
+    root.withdraw()
+    base_path = filedialog.askdirectory(title="Select Base Path")
+    if base_path:
+        global BASE_PATH  # Use the global variable
+        BASE_PATH = base_path
+        sort_videos()
+        print("Video files sorted into frame rate directories.")
+    else:
+        print("No base path selected.")
 
-sort_videos()
+if __name__ == '__main__':
+    select_base_path()
